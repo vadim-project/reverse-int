5ec2704807358cc16ab1864a5db4d4df1031a3af
vadim-project <vadim.rogozhnikov2002@gmail.com>
2023-01-14 21:09:06 +0300

    feat: complete tasks.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 module.exports = function reverse (n) {
-  
+    let g = n.toString();
+    var result = '';
+    for (var i = g.length - 1; i >= 0; i--) {
+        result += g[i];
+    }
+    return parseInt(result);
 }
